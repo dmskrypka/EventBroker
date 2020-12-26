60d7c2324ffba552e8c1ca104be1030b279036fa
Dmytro Skrypka <dmytro.sk@outlook.com>
2020-12-26 14:27:28 +0100

    Add project files.

---
+++ EBroker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EventBroker
+{
+    /// <summary>
+    /// Create using singleton pattern
+    /// </summary>
+    public class EBroker
+    {
+        private readonly MultiDictionary<string, Delegate> subscriptions;
+        public EBroker()
+        {
+            this.subscriptions = new MultiDictionary<string, Delegate>();
+        }
+
+        public void Publish<T>(string name, object sender, T args)
+        {
+            foreach (Delegate handler in subscriptions[name])
+            {
+                handler.DynamicInvoke(sender, args);
+            }
+        }
+
+        public void Subscribe(string name, Delegate handler)
+        {
+            subscriptions.Add(name, handler);
+        }
+    }
+}
---
+++ MultiDictionary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace EventBroker
+{
+    internal class MultiDictionary<TKey, TValue>
+    {
+        private readonly Dictionary<TKey, IList<TValue>> data;
+
+        public IEnumerable<TValue> this[TKey key]
+        {
+            get => this.data[key];
+            set => this.data[key] = new List<TValue>(value);
+        }
+
+        public MultiDictionary()
+        {
+            this.data = new Dictionary<TKey, IList<TValue>>();
+        }
+
+        internal void Add(TKey key, TValue value)
+        {
+            if (this.data.ContainsKey(key))
+                this.data[key].Add(value);
+            else
+                data.Add(key, new List<TValue>() { value });
+        }
+    }
+}
